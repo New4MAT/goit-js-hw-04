36f7e2a6fa6d82a764674c4ef0a2b922cae4e402
alex_matviyuk <alexmatviyuk1@gmail.com>
2025-01-28 21:48:20 +0200

    ready hw4

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,24 @@
-function filterArray(numbers, value) {
-  const filteredNumbers = [];
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-  for (const number of numbers) {
-    if (number > value) {
-      filteredNumbers.push(number);
-    }
-  }
+  changeUsername: function (newName) {
+    this.username = newName;
+  },
 
-  return filteredNumbers;
-}
+  updatePlayTime: function (hours) {
+    this.playTime += hours;
+  },
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+  getInfo: function () {
+    return `${this.username} has ${this.playTime} active hours!`; // Використовуємо this для доступу до властивостей
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
